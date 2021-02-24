e2f3d21141b163c12b35b3185c6648270964d41c
Björn Åström <b@strm.space>
2021-02-24 14:30:02 +0100

    Call Masscan on new C2s

--- probeserve.py
+++ probeserve.py
@@ -1,10 +1,53 @@
 import argparse
+import subprocess
 import mwdblib
 import time
+import pytz
+from datetime import datetime, timedelta
 import re
 import dns.resolver
 
 MWDB_URL = "https://spawnwalk.duckdns.org"
+SCAN_INTERVAL_SECONDS = 10*60
+MASSCAN_PATH = "/home/b/code/masscan/bin/masscan"
+
+# TODO: Perform additional processing to capture
+# objects that do not conform to the below structure.
+# TODO: Fix the shit code.
+def get_c2s(config):
+    cncs = []
+    if config.cfg.get("cncs"):
+        for cnc in config.cfg.get("cncs"):
+            if type(cnc["host"]) is str and type(cnc["port"]) is int:
+                for ip in resolve(cnc["host"]):
+                    print("Received C2:", ip + ":" + str(cnc["port"]))
+                    cncs.append({"ip": ip, "port": cnc["port"]})
+    if config.cfg.get("c2"):
+        for cnc in config.cfg.get("c2"):
+            if type(cnc) is str:
+                cnc_parts = cnc.split(":")
+                for ip in resolve(cnc_parts[0]):
+                    print("Received C2:", ip + ":" + cnc_parts[1])
+                    cncs.append({"ip": ip, "port": cnc_parts[1]})
+    return cncs
+
+
+def init_scan(hosts):
+    print("Running scan")
+
+    ips = []
+    ports = []
+    for host in hosts:
+        ips.append(host["ip"])
+        ports.append(host["port"])
+
+    ips_csv = ",".join(ips)
+    ports_csv = ",".join([ str(x) for x in ports])
+    masscan_cmd = [MASSCAN_PATH, "--redis-queue", "127.0.0.1", "-p", ports_csv, ips_csv]
+    print("Running command '{}'".format(" ".join(masscan_cmd)))
+    subprocess.Popen(masscan_cmd)
+    print("Masscan started")
+
 
 def resolve(address):
     ips = []
@@ -31,22 +74,39 @@ if __name__ == "__main__":
 
     mwdb = mwdblib.MWDB(api_url=args.mwdb_url, api_key=secret)
 
-    for config in mwdb.listen_for_configs():
-        cncs = []
-        # TODO: Perform additional processing to capture
-        # objects that do not conform to the below structure.
-        if config.cfg.get("cncs"):
-            for cnc in config.cfg.get("cncs"):
-                if type(cnc["host"]) is str and type(cnc["port"]) is int:
-                    for ip in resolve(cnc["host"]):
-                        cncs.append(ip + ":" + str(cnc["port"]))
-        if config.cfg.get("c2"):
-            for cnc in config.cfg.get("c2"):
-                if type(cnc) is str:
-                    cnc_parts = cnc.split(":")
-                    for ip in resolve(cnc_parts[0]):
-                        cncs.append(ip + ":" + cnc_parts[1])
+    last_id = None
+    cncs = []
+    while True:
+        if not last_id:
+            print("No last_config_id loaded, get configurations based on date.")
 
-        for cnc in cncs:
-            print("Have CnC:", cnc)
-    time.sleep(3600)
+            # Calculate cutoff time to get configs for the last 24 hours
+            utc=pytz.UTC
+            cutoff_hours = 10
+            cutoff_time =  utc.localize(datetime.utcnow() - timedelta(hours=cutoff_hours))
+
+            recent_configs = mwdb.recent_configs()
+            for idx, config in enumerate(recent_configs):
+                if config.upload_time < cutoff_time:
+                    print("Cutoff time {} reached".format(config.upload_time))
+                    break
+
+                # The first one we receive will be the latest one, store that id
+                if idx == 0:
+                    last_id = config.id
+
+                cncs.extend(get_c2s(config))
+
+        print("Listening for configs")
+        configs = mwdb.listen_for_configs(last_id, blocking=False)
+        start_time = time.localtime()
+        for config in configs:
+            cncs.extend(get_c2s(config))
+
+        if len(cncs) > 0:
+            print("Have CnCs. Scanning")
+            init_scan(cncs)
+            print("Scan finished")
+            cncs = []
+        print("Sleeping for {} seconds".format(SCAN_INTERVAL_SECONDS))
+        time.sleep(SCAN_INTERVAL_SECONDS)
