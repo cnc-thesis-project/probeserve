dabbb691a0d92369bfbe96cecf09c2a72a51e682
pedroingvar <0x696b7559@gmail.com>
2021-03-02 01:07:07 +0100

    Store C&C metadata, Fix stuff Fix incorrect family name retrieval Add mwdb id to cnc list

--- probeserve.py
+++ probeserve.py
@@ -18,11 +18,12 @@ masscan_rate = None
 # objects that do not conform to the below structure.
 def get_c2s(config):
     cncs = []
-    family = config.cfg.get("type")
+    mwdb_id = config.id
+    family = config.family
     for cnc in config.cfg.get("cncs", []):
         if type(cnc["host"]) is str and type(cnc["port"]) is int:
             for ip in resolve(cnc["host"]):
-                cncs.append({"family": family, "ip": ip, "port": cnc["port"]})
+                cncs.append({"id": mwdb_id, "family": family, "ip": ip, "port": cnc["port"]})
     for cnc in config.cfg.get("c2", []):
         if type(cnc) is str:
             url_regex = "^[a-z]+://.*"
@@ -34,7 +35,7 @@ def get_c2s(config):
             continue
         cnc_parts = cnc.split(":")
         for ip in resolve(cnc_parts[0]):
-            cnc = {"family": family, "ip": ip}
+            cnc = {"id": mwdb_id, "family": family, "ip": ip}
             if len(cnc_parts) > 1:
                 cnc["port"] = cnc_parts[1]
             cncs.append(cnc)
@@ -75,6 +76,11 @@ def resolve(address):
 
     return ips
 
+def write_cnc_metadata(cncs, out_path):
+    with open(out_path, "a") as f:
+        for cnc in cncs:
+            f.write("{},{},{},{}\n".format(cnc.get("id"), cnc.get("ip"),
+                    cnc.get("port",""), cnc.get("family", "unknown")))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="The probeserver observer.")
@@ -84,12 +90,14 @@ if __name__ == "__main__":
     parser.add_argument("--cutoff", help="Cutoff time in hours for time based retrieval.", default=24, type=int)
     parser.add_argument("--scan-interval", help="Scan interval.", default=DEFAULT_SCAN_INTERVAL_SECONDS)
     parser.add_argument("--masscan-rate", help="Masscan rate.", default=1000, type=int)
+    parser.add_argument("--out", help="The path to store C&C metadata.", default="cnc.csv", type=str)
     args = parser.parse_args()
     secret = args.secret
     masscan_path = args.masscan
     masscan_rate = args.masscan_rate
     cutoff_hours = args.cutoff
     scan_interval = args.scan_interval
+    out_path = args.out
 
     mwdb = mwdblib.MWDB(api_url=args.mwdb_url, api_key=secret)
 
@@ -123,6 +131,7 @@ if __name__ == "__main__":
 
         if len(cncs) > 0:
             print("Have CnCs. Scanning")
+            write_cnc_metadata(cncs, out_path)
             p = run_scan(cncs)
             time.sleep(scan_interval)
             p.wait()
